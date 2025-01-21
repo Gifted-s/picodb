58e58ba508831f96aed25e436a00cf61b59716b2
Sarthak Makhija <sarthak.makhija@gmail.com>
2025-01-21 13:08:18 +0530

    Sarthak | Runs cargo fmt

--- src/encodex/mod.rs
+++ src/encodex/mod.rs
@@ -1,9 +1,9 @@
 use std::borrow::Cow;
 
 pub(crate) mod bytes_encoder_decoder;
+mod string_encoder_decoder;
 mod u16_encoder_decoder;
 mod u8_encoder_decoder;
-mod string_encoder_decoder;
 
 type BytesNeededForEncoding = usize;
 pub(crate) type EndOffset = usize;
