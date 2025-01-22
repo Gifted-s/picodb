333cc49a72ecfe68ee46107bcdbea0b849c5c01c
Sarthak Makhija <sarthak.makhija@gmail.com>
2025-01-22 16:36:14 +0530

    Sarthak | Adds a test for buffer.flush()

--- src/buffer/buffer.rs
+++ src/buffer/buffer.rs
@@ -23,16 +23,21 @@ impl Buffer {
         }
     }
 
-    fn assign_to_block<'a, PathType: AsRef<Path>>(
+    fn assign_to_block<PathType: AsRef<Path>>(
         &mut self,
         block_id: BlockId,
-        log_manager: &'a mut LogManager<'a, PathType>,
+        log_manager: &mut LogManager<PathType>,
     ) -> Result<(), io::Error> {
         self.flush(log_manager)?;
         self.page = Some(log_manager.file_manager().read::<BufferPage>(&block_id)?);
         self.block_id = Some(block_id);
         self.pins = 0;
         Ok(())
+    }
+
+    fn set_modified(&mut self, transaction_number: isize, log_sequence_number: usize) {
+        self.transaction_number = transaction_number;
+        self.log_sequence_number = log_sequence_number;
     }
 
     fn pin(&mut self) {
@@ -47,12 +52,12 @@ impl Buffer {
         self.pins > 0
     }
 
-    fn flush<'a, PathType: AsRef<Path>>(
+    fn flush<PathType: AsRef<Path>>(
         &mut self,
         log_manager: &mut LogManager<PathType>,
     ) -> Result<(), io::Error> {
         if self.transaction_number >= 0 && self.page.is_some() {
-            let _ = &mut log_manager.flush(self.log_sequence_number)?;
+            let _ = log_manager.flush(self.log_sequence_number)?;
             log_manager.file_manager().write(
                 &self.block_id.as_ref().unwrap(),
                 self.page.as_mut().unwrap().finish(),
@@ -176,5 +181,46 @@ mod tests {
         assert_eq!(0, buffer.pins);
     }
 
-    //TODO: add a test for flushing a page
+    #[test]
+    fn flush_a_buffer() {
+        let file = NamedTempFile::new().expect("Failed to create temp file");
+        let directory_path = file.path().parent().unwrap();
+        let buffer_file_name = file.path().file_name().unwrap().to_str().unwrap();
+        let log_file_name = format!("{}.log", buffer_file_name);
+
+        let file_manager = FileManager::new(directory_path, BLOCK_SIZE).unwrap();
+        let mut log_manager = LogManager::new(&file_manager, log_file_name.to_string()).unwrap();
+
+        assert!(log_manager
+            .file_manager()
+            .append_empty_block(buffer_file_name)
+            .is_ok());
+
+        let mut buffer = Buffer::new();
+        buffer
+            .assign_to_block(BlockId::new(buffer_file_name, 0), &mut log_manager)
+            .unwrap();
+
+        let page = buffer.page.as_mut().unwrap();
+        page.add_u16(250);
+        page.add_string(String::from("BoltDB is a B+Tree based storage engine"));
+
+        let any_transaction_number = 10;
+        let any_log_sequence_number = 100;
+        buffer.set_modified(any_transaction_number, any_log_sequence_number);
+        buffer.flush(&mut log_manager).unwrap();
+
+        buffer
+            .assign_to_block(BlockId::new(buffer_file_name, 0), &mut log_manager)
+            .unwrap();
+
+        let reassigned_buffer_page = buffer.page.unwrap();
+        assert_eq!(250, reassigned_buffer_page.get_u16(0).unwrap());
+        assert_eq!(
+            Some(Cow::Owned(String::from(
+                "BoltDB is a B+Tree based storage engine"
+            ))),
+            reassigned_buffer_page.get_string(1)
+        );
+    }
 }
